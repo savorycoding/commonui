f2f8eb46d40acc7d7d000d4cc13149a9965919bc
savorycoding <savorycode@midasit.com>
2019-11-25 13:30:50 +0900

    Automatic commit of updated project files

--- inair/dest/ja.js
+++ inair/dest/ja.js
@@ -1,5 +1,4 @@
 ﻿let inAirLangData = {
-  "inAir.itvResult.label.immediately": "바로적용됩니땅",
   "inAir.itvResult.label.excellent": "卓越",
   "inAir.itvResult.label.good": "優秀",
   "inAir.itvResult.label.normal_high": "中上",
