547fd3338d66a808b943ab581aa09b4e06042859
savorycoding <54048700+savorycoding@users.noreply.github.com>
2019-11-25 13:29:35 +0900

    Update ko.js

--- inair/ko.js
+++ inair/ko.js
@@ -1,5 +1,4 @@
 let inAirLangData = {
-  "inAir.itvResult.label.immediately": "바로적용됩니땅",
   "inAir.itvResult.label.excellent": "탁월",
   "inAir.itvResult.label.good": "우수",
   "inAir.itvResult.label.normal_high": "보통 상",
