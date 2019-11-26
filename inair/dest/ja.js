e507b18f4916b19874ae8fa162beb63fd58ff60b
savorycoding <savorycode@midasit.com>
2019-11-26 13:52:51 +0900

    Automatic commit of updated project files

--- inair/dest/ja.js
+++ inair/dest/ja.js
@@ -817,7 +817,7 @@
   "inAir.layerPopEMail.guide.msg22":"3. ゲームの場合、案内されるガイドの内容をちゃんと理解した上でマウスまたはキーボードを利用して行ってください。",
   "inAir.layerPopEMail.label.notification2": "[留意事項]",
   "inAir.layerPopEMail.guide.msg23": "1．Google Chromeブラウザでのみ受検できます。",
-  "inAir.layerPopEMail.guide.msg24":"2. 受検を始めた後には中断なしに一回に受検を完了してください。完全に受検に集中できる環境をお先に準備してください。",
+  "inAir.layerPopEMail.guide.msg24":"2. 受検開始後には一時停止などの機能はありません。受検に集中できる環境で開始してください。",
   "inAir.layerPopEMail.guide.msg25":"3. 有線インターネット環境で受検してください。事前にネットワークの連結状態を確認してください。受検途中ネットワークの連結が途切れる場合、受検が中断される可能性があり、その責任はすべて本人にあります。",
   "inAir.layerPopEMail.guide.msg26": "4. 必ず使用中の全てのプログラムを終了してからオンライン面接を進行してください。",
   "inAir.layerPopEMail.guide.msg27": "5. 受検中にはリロード(F5),戻るボタンの使用が不可能です。",
